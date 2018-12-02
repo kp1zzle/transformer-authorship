9cf20f72e08ea05e818cd5dc841905bf9252a55a
Kieran Parikh <kieran@parikh.ca>
2018-12-02 16:23:30 -0500

    Implemented multi-gpu training.

--- transformer-aa.py
+++ transformer-aa.py
@@ -367,6 +367,65 @@ class SimpleLossCompute:
             self.opt.optimizer.zero_grad()
         return loss.data[0] * norm
 
+class MultiGPULossCompute:
+    "A multi-gpu loss compute and train function."
+    def __init__(self, generator, criterion, devices, opt=None, chunk_size=5):
+        # Send out to different gpus.
+        self.generator = generator
+        self.criterion = nn.parallel.replicate(criterion, 
+                                               devices=devices)
+        self.opt = opt
+        self.devices = devices
+        self.chunk_size = chunk_size
+        
+    def __call__(self, out, targets, normalize):
+        total = 0.0
+        generator = nn.parallel.replicate(self.generator, 
+                                                devices=self.devices)
+        out_scatter = nn.parallel.scatter(out, 
+                                          target_gpus=self.devices)
+        out_grad = [[] for _ in out_scatter]
+        targets = nn.parallel.scatter(targets, 
+                                      target_gpus=self.devices)
+
+        # Divide generating into chunks.
+        chunk_size = self.chunk_size
+        for i in range(0, out_scatter[0].size(1), chunk_size):
+            # Predict distributions
+            out_column = [[Variable(o[:, i:i+chunk_size].data, 
+                                    requires_grad=self.opt is not None)] 
+                           for o in out_scatter]
+            gen = nn.parallel.parallel_apply(generator, out_column)
+
+            # Compute loss. 
+            y = [(g.contiguous().view(-1, g.size(-1)), 
+                  t[:, i:i+chunk_size].contiguous().view(-1)) 
+                 for g, t in zip(gen, targets)]
+            loss = nn.parallel.parallel_apply(self.criterion, y)
+
+            # Sum and normalize loss
+            l = nn.parallel.gather(loss, 
+                                   target_device=self.devices[0])
+            l = l.sum()[0] / normalize
+            total += l.data[0]
+
+            # Backprop loss to output of transformer
+            if self.opt is not None:
+                l.backward()
+                for j, l in enumerate(loss):
+                    out_grad[j].append(out_column[j][0].grad.data.clone())
+
+        # Backprop all loss through transformer.            
+        if self.opt is not None:
+            out_grad = [Variable(torch.cat(og, dim=1)) for og in out_grad]
+            o1 = out
+            o2 = nn.parallel.gather(out_grad, 
+                                    target_device=self.devices[0])
+            o1.backward(gradient=o2)
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return total * normalize
+
 # Greedy decoder
 def greedy_decode(model, src, src_mask, max_len, start_symbol):
     memory = model.encode(src, src_mask)
@@ -433,7 +492,6 @@ def make_model(src_vocab, tgt_vocab, N=6,
     return model
 
 ### LOAD DATASET AND TRAIN MODEL ###
-use_cuda = True
 # Load dataset
 train_file = 'C50-trainData.csv'
 test_file = 'C50-testData.csv'
@@ -465,13 +523,12 @@ print("Vocabulary built")
 print("Source vocab length: " + str(len(SRC.vocab)))
 print("Target vocab length: " + str(len(TGT.vocab)))
 
+devices = [0]
 model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
-pad_idx = SRC.vocab.stoi["<blank>"].cuda()
+model.cuda()
+pad_idx = SRC.vocab.stoi["<blank>"]
 criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
-
-if use_cuda:
-    model.cuda()
-    criterion.cuda()
+criterion.cuda()
 
 
 BATCH_SIZE = 100
@@ -483,6 +540,8 @@ test_iter = MyIterator(test, batch_size=BATCH_SIZE, device=0,
                         repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
                         batch_size_fn=batch_size_fn, train=False)
 
+model_par = nn.DataParallel(model, device_ids=devices)
+
 # Train model
 model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
             torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
@@ -492,7 +551,8 @@ print("Training...")
 for epoch in range(10):
     print("Epoch " + str(epoch) + ":")
     loss = run_epoch((rebatch(pad_idx, b) for b in train_iter), 
-                  model, SimpleLossCompute(model.generator, criterion, model_opt))
+                  model_par, 
+                  MultiGPULossCompute(model.generator, criterion, devices=devices, opt=model_opt))
     print(loss)
 
 
