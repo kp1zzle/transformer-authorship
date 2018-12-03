b470289fb1bd85d9a0bc8f1ffda4ab90f8bbaef3
Kieran Parikh <kieran@parikh.ca>
2018-12-02 23:25:35 -0500

    Implemented argparser

--- transformer-aa.py
+++ transformer-aa.py
@@ -499,9 +499,9 @@ def make_model(src_vocab, tgt_vocab, N=6,
 ### LOAD DATASET AND TRAIN MODEL ###
 # Parse command line args
 parser = argparse.ArgumentParser(description='Train transformer for authorship attribution.')
-parser.add_argument('--stack-layers', type=int, default=6, help='num encoder layers')
+parser.add_argument('--stack_layers', type=int, default=6, help='num encoder layers')
 parser.add_argument('--epochs', type=int, default=10, help='num training epochs')
-args = vars(parser.parse_args())
+args = parser.parse_args()
 
 # Load dataset
 train_file = 'C50-trainData.csv'
@@ -532,7 +532,7 @@ print("Source vocab length: " + str(len(SRC.vocab)))
 print("Target vocab length: " + str(len(TGT.vocab)))
 
 devices = [0]
-model = make_model(len(SRC.vocab), len(TGT.vocab), N=args['stack-layers'])
+model = make_model(len(SRC.vocab), len(TGT.vocab), N=args.stack_layers)
 model = model.cuda()
 pad_idx = TGT.vocab.stoi["<blank>"]
 criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
@@ -552,7 +552,7 @@ model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
 
 
 print("Training...")
-for epoch in range(args['epochs']):
+for epoch in range(args.epochs):
     print("Epoch " + str(epoch) + ":")
     model_par.train()
     run_epoch((rebatch(pad_idx, b) for b in train_iter), 
